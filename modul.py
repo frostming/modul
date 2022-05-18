2ead9d537178bf8497f8a5ff58f5b830f448ba4f
Frost Ming <mianghong@gmail.com>
2022-05-18 21:25:37 +0800

    Implement dict APIs for modul.exports

--- modul.py
+++ modul.py
@@ -3,7 +3,17 @@ from __future__ import annotations
 import inspect
 import sys
 import types
-from typing import Any, Mapping, TypeVar, overload
+from typing import (
+    Any,
+    Collection,
+    Iterable,
+    Iterator,
+    Mapping,
+    MutableMapping,
+    Sequence,
+    TypeVar,
+    overload,
+)
 
 _Decorated = TypeVar("_Decorated")
 
@@ -12,108 +22,152 @@ class DuplicateExportName(ValueError):
     pass
 
 
-class Exports:
-    """A singleton instance that holds the exported members for all modules."""
+class Exports(MutableMapping[str, Any]):
+    __namespace__: dict[str, Any]
 
-    _namespaces: dict[types.ModuleType, dict[str, Any]] = {}
+    def __init__(self, namespace: dict[str, Any]) -> None:
+        super().__setattr__("__namespace__", namespace)
 
-    def get_current_module(self, stacklevel: int = 0) -> types.ModuleType:
-        stack = inspect.stack()
-        # stack[0] is this function, stack[1] is the caller export_var
-        # stack[2] is the caller of export_var, stack[3] is the target module
-        module = inspect.getmodule(stack[3 + stacklevel][0])
-        return module
-
-    def clear_namespace(self, stacklevel: int = 0) -> None:
-        module = self.get_current_module(stacklevel)
-        self._namespaces.pop(module, None)
-
-    def export_var(self, name: str, value: Any, stacklevel: int = 0) -> None:
-        module = self.get_current_module(stacklevel)
-        namespace = self._namespaces.setdefault(module, {})
+    def __setitem__(self, name: str, value: Any) -> None:
+        if not isinstance(name, str):
+            raise TypeError(f"Exported name {name} must be a string")
+        namespace = self.__namespace__
         if name in namespace:
             raise DuplicateExportName(f"{name} has already been exported")
         namespace[name] = value
-        if hasattr(module, "__all__"):
-            module.__all__.append(name)
-        self._replace_module(module)
 
     @overload
-    def __call__(
-        self, to_export: _Decorated, clear: bool = ..., stacklevel: int = ...
-    ) -> _Decorated:
+    def __call__(self, to_export: _Decorated) -> _Decorated:
         ...
 
     @overload
-    def __call__(
-        self, to_export: Mapping[str, Any], clear: bool = ..., stacklevel: int = ...
-    ) -> None:
+    def __call__(self, to_export: Mapping[str, Any]) -> None:
         ...
 
     def __call__(
         self,
         to_export: _Decorated | Mapping[str, Any],
-        clear: bool = False,
-        stacklevel: int = 0,
     ) -> Any:
-        if clear:
-            self.clear_namespace(stacklevel)
         if isinstance(to_export, Mapping):
-            for name, value in to_export.items():
-                if not isinstance(name, str):
-                    raise TypeError(f"Exported name {name} must be a string")
-                self.export_var(name, value, stacklevel)
-            return None
+            return self.update(to_export)
         if not hasattr(to_export, "__name__"):
             raise TypeError(
                 "The exported object must be a mapping or have __name__ attribute"
             )
-        self.export_var(to_export.__name__, to_export, stacklevel)
+        self[to_export.__name__] = to_export
         return to_export
 
     def __setattr__(self, name: str, value: Any) -> None:
-        self.export_var(name, value)
+        self[name] = value
 
-    def __setitem__(self, name: str, value: Any) -> None:
-        self.export_var(name, value)
+    def __getitem__(self, name: str) -> Any:
+        return self.__namespace__[name]
 
-    def _replace_module(self, module: types.ModuleType) -> None:
-        this = self
-        if getattr(module, "__export__", False):
-            return
+    def __getattr__(self, name: str) -> Any:
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
 
-        class _ExportsModule(types.ModuleType):
-            # Set `__all__` to a KeyView to keep track of subsequent additions
-            __all__ = list(this._namespaces.setdefault(module, {}))
-            __export__ = True
-            __file__ = module.__file__
+    def __delitem__(self, name: str) -> None:
+        del self.__namespace__[name]
+
+    def __iter__(self) -> Iterator[str]:
+        return iter(self.__namespace__)
+
+    def __len__(self) -> int:
+        return len(self.__namespace__)
+
+    def __repr__(self) -> str:
+        return f"Exports({self.__namespace__!r})"
+
+
+class SequenceProxy(Sequence[str]):
+    def __init__(self, data: Collection) -> None:
+        self.data = data
+
+    def __getitem__(self, index: int) -> str:
+        return list(self.data)[index]
+
+    def __iter__(self) -> Iterator[str]:
+        return iter(self.data)
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __repr__(self) -> str:
+        return f"SequenceProxy({list(self.data)!r})"
+
+    def __eq__(self, __o: object) -> bool:
+        return list(self) == __o
+
+
+class _ExportRegistry:
+    """A singleton instance that holds the exported members for all modules."""
+
+    def __init__(self) -> None:
+        self.namespaces: dict[types.ModuleType, dict[str, Any]] = {}
+
+    def get_current_module(self) -> types.ModuleType:
+        stack = inspect.stack()
+        # stack[0] is this method; stack[1] is the caller `__get__` / `__set__`
+        # stack[2] is the target module
+        module = inspect.getmodule(stack[2][0])
+        if module is None:
+            module = sys.modules["__main__"]
+        return self._replace_module(module)
+
+    def _replace_module(self, module: types.ModuleType) -> types.ModuleType:
+        if getattr(module, "__exported__", False):
+            return module
+
+        namespace = self.namespaces.pop(module, {})
+
+        class _ExportedModule(types.ModuleType):
+            __file__ = getattr(module, "__file__", None)
+            __exported__ = True
+            __all__ = SequenceProxy(namespace)
 
             def __getattr__(self, name: str) -> Any:
+                if not namespace:
+                    # If the module doesn't export any members, it will act like before.
+                    return getattr(module, name)
                 try:
-                    return this._namespaces.get(self, {})[name]
+                    return namespace[name]
                 except KeyError:
                     raise AttributeError(
-                        f"Module {self.__name__} has no attribute {name}"
+                        f"Module {module.__name__!r} has no attribute {name!r}"
                     ) from None
 
-        newmodule = _ExportsModule(module.__name__, module.__doc__)
-        this._namespaces[newmodule] = this._namespaces.pop(module, None)
+            def __dir__(self) -> Iterable[str]:
+                if not namespace:
+                    return dir(module)
+                return sorted(namespace) + super().__dir__()
+
+        newmodule = _ExportedModule(module.__name__, getattr(module, "__doc__", None))
         sys.modules[module.__name__] = newmodule
+        self.namespaces[newmodule] = namespace
+        return newmodule
 
+    def __get__(self, instance: Any, owner: Any) -> Exports:
+        if instance is None:
+            return self
+        module = self.get_current_module()
+        namespace = self.namespaces.setdefault(module, {})
+        return Exports(namespace)
 
-exports = Exports()
+    def __set__(self, instance: Any, value: Any) -> None:
+        module = self.get_current_module()
+        namespace = self.namespaces.setdefault(module, {})
+        Exports(namespace)(value)
 
 
 class _ModuleExports(types.ModuleType):
     __file__ = __file__
     __all__ = ["exports"]
-    exports = exports
-
-    def __setattr__(self, name: str, value: Any) -> None:
-        if name != "exports":
-            super().__setattr__(name, value)
-        self.exports(value, clear=True, stacklevel=1)
+    exports = _ExportRegistry()
 
 
+exports: Exports
 sys.modules[__name__] = _ModuleExports(__name__, __doc__)
-del Exports
+del _ModuleExports, _ExportRegistry
